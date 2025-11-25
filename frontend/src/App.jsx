a4519bc583271e0733506a5e22a4766ab87ab770
gillsatwinder <satwinders0504@gmail.com>
2025-11-25 09:35:01 -0500

    deleting duplicate files

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,3 @@
-<<<<<<< HEAD
-import React from "react";
-import { Routes, Route } from "react-router-dom";
-import Homepage from "./Homepage.jsx";
-import Signin from "./Signin.jsx";
-import Signup from "./Signup.jsx";
-import Dashboard from "./Dashboard.jsx";
-
-=======
 import { Routes, Route } from "react-router-dom";
 import Homepage from "./pages/Homepage.jsx";
 import Signin from "./pages/Signin.jsx";
@@ -14,24 +5,18 @@ import Signup from "./pages/Signup.jsx";
 import Dashboard from "./pages/Dashboard.jsx";
 import AllDecksView from "./pages/AllDecksView.jsx"; 
 import DeckPage from "./pages/DeckPage.jsx";
->>>>>>> origin/main
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Homepage />} />
       <Route path="/signin" element={<Signin />} />
-<<<<<<< HEAD
-       <Route path="/signup" element={<Signup />} />
-      <Route path="/dashboard" element={<Dashboard />} />
-=======
       <Route path="/signup" element={<Signup />} />
 
       <Route path="/dashboard" element={<Dashboard />}>
         <Route index element={<AllDecksView />} /> 
         <Route path="deck/:deckId" element={<DeckPage />} /> 
       </Route>
->>>>>>> origin/main
     </Routes>
   );
 }
